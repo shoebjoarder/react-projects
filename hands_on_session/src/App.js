e2aaeee63d7ae8275b5f59ad2ab541c39000ca24
Shoeb Joarder <shoebjoarder@outlook.com>
2020-05-08 15:55:03 +0200

    Added header, body and defined states

--- hands_on_session/src/App.js
+++ hands_on_session/src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react'
+import Header from './layout/Header'
+import Body from './layout/Body'
 
 export class App extends Component {
+  // Initialize the states
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Do the AWT Bootstrap & MongoDB Homework by 20th of May',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Finish the second draft of the presentation',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Finish the recordings of the presentation',
+        completed: false
+      },
+      {
+        id: 4,
+        title: 'Prepare the hands-on session codes',
+        completed: false
+      },
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <h1>
-          Hello World;
-        </h1>
+        <Header />
+        <Body />
       </div>
     )
   }
